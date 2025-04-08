77223de52ff31f8cf52d580e52083e7615b56270
Nadelio <carefulsniffle@gmail.com>
2025-04-07 20:48:41 -0500

    Removing unnecessary files, compiling to .jar

--- SimpleDebug.java
+++ SimpleDebug.java
@@ -8,6 +8,10 @@ public class SimpleDebug {
 
     private int DEBUG;
 
+    /**
+     * Create a debugger instance with a specified debug level.
+     * @param debugLevel
+     */
     public SimpleDebug(int debugLevel) { this.DEBUG = debugLevel; }
     public void setDebugLevel(int debugLevel) { this.DEBUG = debugLevel; }
     public void disable() { this.DEBUG = 0; }
@@ -32,17 +36,35 @@ public class SimpleDebug {
         logger.warn(logger.info(2, "This is a warning message with info", 42), 2);
     }
 
+    /**
+     * Send a debug message to the console.
+     * @param message
+     * @param level
+     */
     public void debug(String message, int level) {
         if(this.DEBUG < level) return;
         System.out.println(SimpleColor.DEBUG("[ DEBUG: " + message) + SimpleColor.DEBUG(" ]"));
     }
 
+    /**
+     * Send a debug message to the console and log it to a file.
+     * @param message
+     * @param file
+     * @param level
+     * @throws Exception
+     */
     public void debug(String message, File file, int level) throws Exception {
         if(this.DEBUG < level) return;
         debug(message, level);
         log(message, file, level);
     }
 
+    /**
+     * Send a debug message to the console with additional information.
+     * @param message
+     * @param level
+     * @param args
+     */
     public void debug(String message, int level, Object... args) {
         if(this.DEBUG < level) return;
         if (args.length == 0) {
@@ -59,6 +81,12 @@ public class SimpleDebug {
         System.out.println(SimpleColor.DEBUG("[ DEBUG: " + message + "|") + sb.toString() + SimpleColor.DEBUG(" ]"));
     }
 
+    /**
+     * Format a set of arguments into a string with info color.
+     * @param level
+     * @param args
+     * @return
+     */
     public String info(int level, Object... args) {
         if(this.DEBUG < level || args.length == 0) return "";
 
@@ -72,26 +100,57 @@ public class SimpleDebug {
         return sb.toString();
     }
 
+    /**
+     * Send a warning message to the console.
+     * @param level
+     * @param args
+     * @return
+     */
     public void warn(String message, int level) {
         if(this.DEBUG < level) return;
         System.out.println(SimpleColor.WARNING("[ WARN: " + message) + SimpleColor.WARNING(" ]"));
     }
 
+    /**
+     * Send a notice message to the console.
+     * @param level
+     * @param args
+     * @return
+     */
     public void notice(String message, int level) {
         if(this.DEBUG < level) return;
         System.out.println(SimpleColor.NOTICE("[ NOTICE: " + message) + SimpleColor.NOTICE(" ]"));
     }
 
+    /**
+     * Send an error message to the console.
+     * @param level
+     * @param args
+     * @return
+     */
     public void error(String message, int level) {
         if(this.DEBUG < level) return;
         System.out.println(SimpleColor.ERROR("[ ERROR: ") + SimpleColor.INFO(message) + SimpleColor.ERROR(" ]"));
     }
 
+    /**
+     * Send a fatal error message to the console and throw an exception.
+     * @param level
+     * @param args
+     * @throws Exception
+     */
     public void fatal(String message, int level) throws Exception {
         if(this.DEBUG < level) return;
         throw new Exception(SimpleColor.ERROR("[ FATAL: ") + SimpleColor.INFO(message) + SimpleColor.ERROR(" ]"));
     }
 
+    /**
+     * Log a message to a file with a timestamp.
+     * @param message
+     * @param file
+     * @param level
+     * @throws Exception
+     */
     public void log(String message, File file, int level) throws Exception {
         if(this.DEBUG < level) return;
 
